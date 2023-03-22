26ba0638e3c11f358f06069ef97bd10e508d400e
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2023-03-22 20:08:29 +0100

    Starting to work with laravel

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,15 @@
+<nav class="w-100 h-10 p-2 border-bottom d-flex justify-content-between">
+    <img style="height: 3rem;" src="img/logo.png" alt="logo" />
+    <nav class="nav">
+        {{-- prettier-ignore --}}
+            <a class="nav-link" href="#"><i style="margin-right: .5rem;" class="fa-solid fa-house ml-2"></i>Home</a>
+        {{-- prettier-ignore --}}
+            <a class="nav-link" href="#"><i style="margin-right: .5rem;" class="fa-sharp fa-solid fa-door-closed"></i>Log out</a>
+        {{-- prettier-ignore --}}
+            <a class="nav-link" href="#"><i style="margin-right: .5rem;" class="fa-solid fa-door-open"></i>Register</a>
+        {{-- prettier-ignore --}}
+            <a class="nav-link" href="#"><i style="margin-right: .5rem;" class="fa-solid fa-door-open"></i>Login</a>
+        {{-- prettier-ignore --}}
+            <a class="nav-link" href="#"><i style="margin-right: .5rem;" class="fa-solid fa-user"></i>My account</a>
+    </nav>
+</nav>
